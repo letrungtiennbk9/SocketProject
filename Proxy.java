8a8cf4a76e02d47f2416fbb882228f8d01059d65
letrungtiennbk9 <47937082+letrungtiennbk9@users.noreply.github.com>
2019-05-05 10:07:16 +0700

    Update Proxy.java

--- Proxy.java
+++ Proxy.java
@@ -37,21 +37,5 @@ Mỗi khi server đóng, tất cả dữ liệu về những trang bị block ho
 điều này giúp Proxy có thể luôn duy trì, cập nhật và lưu trữ chúng.
 */
 public class Proxy implements Runnable {    //Tạo class Runnable vì Proxy là đối tượng được thực thi trên Thread
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-
-
-
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	
 }
